e440ea4bbfd0b549dae59a5139f06da59929e45a
Adrien <adrien.dubois35@gmail.com>
2020-01-23 11:25:51 -0500

    Crawl multiple pages

--- cralwer.js
+++ cralwer.js
@@ -1,33 +1,58 @@
-// Packages
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
+const parallel = 4;
 const fs = require("fs");
 
-// User defined variables
+
+
 const pages = JSON.parse(fs.readFileSync("pages.json")).pages;
 
-console.log("Crawling 🚀");
+const screenshotPages = async (pages, parallel) => {
+	const parallelBatches = Math.ceil(pages.length / parallel)
 
-if (!fs.existsSync("screenshots")){
-	fs.mkdirSync("screenshots");
-}
+	console.log('\nI have gotten the task of taking screenshots of ' + pages.length + ' Wikipedia articles on pages in Cologne and will take ' + parallel + ' of them in paralell.')
 
-for (const page of pages) (
-	console.log(page)
-);
+	console.log(' This will result in ' + parallelBatches + ' batches.')
 
+	// Split up the Array of pages
+	let k = 0
+	for (let i = 0; i < pages.length; i += parallel) {
+	  k++
+	  console.log('\nBatch ' + k + ' of ' + parallelBatches)
+	  // Launch and Setup Chromium
+	  const browser = await puppeteer.launch();
+	  // Fun with puppeteer
+	  const context = await browser.createIncognitoBrowserContext();
+	  const page = await context.newPage();
+	  page.setJavaScriptEnabled(false)
 
-(async () => {
-	try {
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
-		await page.goto(pages[0]);
-		await page.screenshot({path: "screenshots/" + pages[0].replace("https://","") + ".png"});
+	  const promises = []
+	  for (let j = 0; j < parallel; j++) {
+		let elem = i + j
+		// only proceed if there is an element
+		if (pages[elem] != undefined) {
+		  // Promise to take Screenshots
+		  // promises push
+		  console.log('🖖 I promise to screenshot: ' + pages[elem].name)
+		  promises.push(browser.newPage().then(async page => {
+			await page.setViewport({ width: 1280, height: 800 })
+			try {
+			  // Only create screenshot if page.goto get's no error
+			  await page.goto(pages[elem])
+			  await page.screenshot({ path: elem + ' ' + pages[elem].name +'.png' }).then(console.log('🤞 I have kept my promise to screenshot ' + pages[elem].name))
+			} catch (err) {
+			  console.log('❌ Sorry! I couldn\'t keep my promise to screenshot ' + pages[elem].name)
+			}
+		  }))
+		}
+	  }
 
-		await browser.close();
-	} catch (error) {
-		console.log(error);
+	  // await promise all and close browser
+	  await Promise.all(promises)
+	  await browser.close()
+
+	  console.log('\nI finished this batch. I\'m ready for the next batch');
 	}
-})();
+  }
 
+  screenshotPages(pages, parallel)
 
-console.log("Crawling complete 👍");
