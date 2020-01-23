a0e899392427f198574483849bbfae7b2e4a19e5
Adrien <adrien.dubois35@gmail.com>
2020-01-23 10:55:28 -0500

    Create folder using fs

--- cralwer.js
+++ cralwer.js
@@ -7,21 +7,27 @@ const pages = JSON.parse(fs.readFileSync("pages.json")).pages;
 
 console.log("Crawling 🚀");
 
+if (!fs.existsSync("screenshots")){
+	fs.mkdirSync("screenshots");
+}
+
 for (const page of pages) (
 	console.log(page)
 );
+
 
 (async () => {
 	try {
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
-		await page.goto("https://example.com");
-		await page.screenshot({path: "screenshots/example.png"});
+		await page.goto(pages[0]);
+		await page.screenshot({path: "screenshots/" + pages[0].replace("https://","") + ".png"});
 
 		await browser.close();
 	} catch (error) {
 		console.log(error);
 	}
 })();
+
 
 console.log("Crawling complete 👍");
