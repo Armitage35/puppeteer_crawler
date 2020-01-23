34415e833615a14fa39ed469ef17178386cd07c1
Restyled.io <commits@restyled.io>
2020-01-23 16:56:06 +0000

    Restyled by prettier

--- cralwer.js
+++ cralwer.js
@@ -1,70 +1,93 @@
 const puppeteer = require('puppeteer');
 const parallel = 4;
 
-const fs = require("fs");
-const pages = JSON.parse(fs.readFileSync("pages.json")).pages;
+const fs = require('fs');
+const pages = JSON.parse(fs.readFileSync('pages.json')).pages;
 
-
-if (!fs.existsSync("screenshots")){
-    fs.mkdirSync("screenshots");
-};
+if (!fs.existsSync('screenshots')) {
+	fs.mkdirSync('screenshots');
+}
 
 const screenshotPages = async (pages, parallel) => {
+	console.log('Crawling launched 🚀');
+	console.log('');
 
-	console.log("Crawling launched 🚀");
-	console.log("");
+	const parallelBatches = Math.ceil(pages.length / parallel);
 
-	const parallelBatches = Math.ceil(pages.length / parallel)
+	console.log(
+		'\nI have gotten the task of taking screenshots of ' +
+			pages.length +
+			' pages and will take ' +
+			parallel +
+			' of them in paralell.'
+	);
 
-	console.log('\nI have gotten the task of taking screenshots of ' + pages.length + ' pages and will take ' + parallel + ' of them in paralell.')
-
-	console.log(' This will result in ' + parallelBatches + ' batches.')
+	console.log(' This will result in ' + parallelBatches + ' batches.');
 
 	// Split up the Array of pages
-	let k = 0
+	let k = 0;
 
 	for (let i = 0; i < pages.length; i += parallel) {
-	  k++
-	  console.log('\nBatch ' + k + ' of ' + parallelBatches)
-	  // Launch and Setup Chromium
-	  const browser = await puppeteer.launch();
-	  // Fun with puppeteer
-	  const context = await browser.createIncognitoBrowserContext();
-	  const page = await context.newPage();
-	  page.setJavaScriptEnabled(false)
+		k++;
+		console.log('\nBatch ' + k + ' of ' + parallelBatches);
+		// Launch and Setup Chromium
+		const browser = await puppeteer.launch();
+		// Fun with puppeteer
+		const context = await browser.createIncognitoBrowserContext();
+		const page = await context.newPage();
+		page.setJavaScriptEnabled(false);
 
-	  const promises = []
-	  for (let j = 0; j < parallel; j++) {
-		let elem = i + j
-		// only proceed if there is an element
-		if (pages[elem] != undefined) {
-		  // Promise to take Screenshots
+		const promises = [];
+		for (let j = 0; j < parallel; j++) {
+			let elem = i + j;
+			// only proceed if there is an element
+			if (pages[elem] != undefined) {
+				// Promise to take Screenshots
 
-		  console.log('🖖 I promise to screenshot: ' + pages[elem])
-		  promises.push(browser.newPage().then(async page => {
-			await page.setViewport({ width: 1280, height: 800 })
-			try {
-				// Only create screenshot if page.goto get's no error
-				await page.goto(pages[elem])
-				const pageTitle = await page.title();
+				console.log('🖖 I promise to screenshot: ' + pages[elem]);
+				promises.push(
+					browser.newPage().then(async page => {
+						await page.setViewport({ width: 1280, height: 800 });
+						try {
+							// Only create screenshot if page.goto get's no error
+							await page.goto(pages[elem]);
+							const pageTitle = await page.title();
 
-			  await page.screenshot({ path: "screenshots/" + elem + " " + pageTitle +'.png' }).then(console.log('🤞 I have kept my promise to screenshot ' + pages[elem]))
-			} catch (err) {
-			  console.log('❌ Sorry! I couldn\'t keep my promise to screenshot ' + pages[elem])
+							await page
+								.screenshot({
+									path:
+										'screenshots/' +
+										elem +
+										' ' +
+										pageTitle +
+										'.png',
+								})
+								.then(
+									console.log(
+										'🤞 I have kept my promise to screenshot ' +
+											pages[elem]
+									)
+								);
+						} catch (err) {
+							console.log(
+								"❌ Sorry! I couldn't keep my promise to screenshot " +
+									pages[elem]
+							);
+						}
+					})
+				);
 			}
-		  }))
 		}
-	  }
 
-	  // await promise all and close browser
-	  await Promise.all(promises)
-	  await browser.close()
+		// await promise all and close browser
+		await Promise.all(promises);
+		await browser.close();
 
-	  console.log('\nI finished this batch. I\'m ready for the next batch');
+		console.log("\nI finished this batch. I'm ready for the next batch");
 	}
 
-	console.log("");
-	console.log("Crawling completed 👍");
-  }
+	console.log('');
+	console.log('Crawling completed 👍');
+};
 
-  screenshotPages(pages, parallel)
+screenshotPages(pages, parallel);
