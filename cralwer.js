38e8e6904849308b9eeb579ddb45476ab318c14d
Adrien <adrien.dubois35@gmail.com>
2020-01-23 10:28:14 -0500

    add the notion of pages

--- cralwer.js
+++ cralwer.js
@@ -1,12 +1,21 @@
 const puppeteer = require("puppeteer");
+const pages = fs.readFileSync;
 
 console.log("Crawling 🚀");
 
-(async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto("https://example.com");
-	await page.screenshot({path: "screenshots/example.png"});
+console.log(pages);
 
-	await browser.close();
+(async () => {
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto("https://example.com");
+		await page.screenshot({path: "screenshots/example.png"});
+
+		await browser.close();
+	} catch (error) {
+		console.log(error);
+	}
 })();
+
+console.log("Crawling complete 👍");
